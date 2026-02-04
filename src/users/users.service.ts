d0e7c6c77899a75ac2eb9806b3dbe771f893e7f4
Esteban Garrido <esdagava@gmail.com>
2026-02-03 22:30:53 -0500

    Users/Auth

---
+++ src/users/users.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+  getAllUsers() {
+    return this.usersRepository.getAllUsers();
+  }
+}
