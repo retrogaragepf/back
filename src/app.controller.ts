deefcfdd7a51f1b9304b32dfc2637233f2012a6d
MarceloCanda <marcelocanda254@gmail.com>
2026-02-17 21:24:02 -0300

    stripe prueba 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('success')
+  success() {
+    return 'Pago exitoso ✅';
+  }
+
+  @Get('cancel')
+  cancel() {
+    return 'Pago cancelado ❌';
+  }
 }
